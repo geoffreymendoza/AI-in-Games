d3e753e038602b03f5ce8b15e5b14319dc3ac48a
Dyep <geoffrey.mendoza@ciit.edu.ph>
2023-09-21 12:50:50 +0800

    -added lesson offlink curve

---
+++ Assets/Scripts/Lessons/LessonOfflinkCurve.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class LessonOfflinkCurve : MonoBehaviour {
+    private NavMeshAgent agent;
+    public OfflinkMoveType moveType;
+    public AnimationCurve animCurve;
+    public float height = 2.0f;
+    public float duration = 0.5f;
+
+    private void Awake() {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Start is called before the first frame update
+    IEnumerator Start() {
+        agent.autoTraverseOffMeshLink = false;
+        while (true) {
+            if(agent.isOnOffMeshLink) {
+                switch (moveType) {
+                    case OfflinkMoveType.Invalid:
+                        break;
+                    case OfflinkMoveType.Normal:
+                        yield return StartCoroutine(NormalSpeed(agent));
+                        break;
+                    case OfflinkMoveType.Parabola:
+                        yield return StartCoroutine(Parabola(agent, height, duration));
+                        break;
+                    case OfflinkMoveType.Curve:
+                        yield return StartCoroutine(Curve(agent, duration));
+                        break;
+                }
+                agent.CompleteOffMeshLink();
+            }
+            yield return null;
+        }
+    }
+
+    // Derives the speed
+    private IEnumerator NormalSpeed(NavMeshAgent agent) {
+        OffMeshLinkData linkData = agent.currentOffMeshLinkData;
+        Vector3 endPos = linkData.endPos + Vector3.up * agent.baseOffset;
+        while (agent.transform.position != endPos) {
+            agent.transform.position = Vector3.MoveTowards(agent.transform.position, endPos, agent.speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    // fixed height and
+    private IEnumerator Parabola(NavMeshAgent agent, float height, float duration) {
+        OffMeshLinkData linkData = agent.currentOffMeshLinkData;
+        Vector3 startPos = agent.transform.position;
+        Vector3 endPos = linkData.endPos + Vector3.up * agent.baseOffset;
+        float normalizeTime = 0.0f;
+        while(normalizeTime <= 1) {
+            float yOffset = height * 4.0f * (normalizeTime - normalizeTime * normalizeTime);
+            agent.transform.position = Vector3.Lerp(startPos, endPos, yOffset) + yOffset * Vector3.up;
+            normalizeTime += Time.deltaTime / duration;
+            yield return null;
+        }
+    }
+
+    // depends on curve animation
+    private IEnumerator Curve(NavMeshAgent agent, float duration) {
+        OffMeshLinkData linkData = agent.currentOffMeshLinkData;
+        Vector3 startPos = agent.transform.position;
+        Vector3 endPos = linkData.endPos + Vector3.up * agent.baseOffset;
+        float normalizeTime = 0.0f;
+        while(normalizeTime <= 1) {
+            float yOffset = animCurve.Evaluate(normalizeTime);
+            agent.transform.position = Vector3.Lerp(startPos, endPos, yOffset) + yOffset * Vector3.up;
+            normalizeTime += Time.deltaTime / duration;
+            yield return null;
+        }
+    }
+}
+
+
+public enum OfflinkMoveType {
+    Invalid = -1,
+    Normal,
+    Teleport,
+    Parabola,
+    Curve,
+}
